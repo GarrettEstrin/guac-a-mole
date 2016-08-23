0fd5f6b1491dbf9d69125c9a67244839f52b8a04
Garrett Estrin <garrett.estrin@gmail.com>
2016-08-23 12:14:29 -0700

    added game objects

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
 // Customer Javascript by Garrett Estrin
 
-// Score Counter
-var score = 0;
-var scoreBoard = document.getElementById('score-board');
-// create container variable
-var theContainer = document.getElementById('container');
-// create box variables. id=boxNum
-var holes = document.querySelectorAll('.hole');
 
+// Game object
+var game = {};
+
+
+// Score Counter
+game.score = 0;
+game.scoreBoard = document.getElementById('score-board');
+// create container variable
+game.theContainer = document.getElementById('container');
+// create box variables. id=boxNum
+game.holes = document.querySelectorAll('.hole');
+
+game.utils = {};
 // Timer
 
 // Mole
-var mole = '<img width="60px" src="mole2.png" alt="mole" align="center" />';
+game.mole = '<img width="60px" src="mole2.png" alt="mole" align="center" />';
 // functions
 
 // Place mole in random hole at start
-var firstHole = function(){
-  holes[randomInt(9)].innerHTML = mole;
+game.utils.firstHole = function(){
+  game.holes[game.utils.randomInt(9)].innerHTML = game.mole;
 }
 // Random number function
-function randomInt(n){
+game.utils.randomInt = function(n){
   return Math.floor(n * Math.random())
 }
 
 
 // Functions called on page load
-firstHole();
+game.utils.firstHole();
 
 // clicklisteners for boxes
-holes.forEach(function(el){
+game.holes.forEach(function(el){
   el.addEventListener('click', function(){
     if(el.innerHTML === ""){
       console.log('miss');
-      score = score - 100;
-      scoreBoard.innerHTML = score;
+      game.score = game.score - 100;
+      game.scoreBoard.innerHTML = game.score;
     }
     else {
       console.log('hit');
-      score = score + 100;
-      console.log(score);
+      game.score = game.score + 100;
+      console.log(game.score);
       el.innerHTML = "";
-      scoreBoard.innerHTML = score;
-      holes[randomInt(9)].innerHTML = mole;
+      game.scoreBoard.innerHTML = game.score;
+      game.holes[game.utils.randomInt(9)].innerHTML = game.mole;
     }
   })
 });
